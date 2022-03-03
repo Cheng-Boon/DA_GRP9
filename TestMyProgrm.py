da13379d157f4689b780cffad5fb1cf32e56284b
DA__GRP9 <goh_cheng_boon@connect.ite.edu.sg>
2022-03-03 23:47:53 +0800

    Updated

---
+++ TestMyProgrm.py
@@ -0,0 +1,13 @@
+# Task 8
+import unittest
+import main as prog
+
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_task8(self):
+        print("Testing")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
+import scrapy
 
+URL = 'https://www.lego.com/en-sg/themes/marvel'
+# Task5.1
 url = 'http://www.wikipedia.org'
 r = requests.get(url)
 print(r.text)
+print("**********")
 
+# Task 5.2
 print("Status.code.")
 print("\t*", r.status_code)
+if r.status_code == 200:
+    print("OK!")
+else:
+    print("Error")
 
+# Task 5.3 & 5.4
 h = requests.head(url)
-print("Header.")
 print("***********")
+print("Header.")
 for x in h.headers:
     print("\t", x, ":", h.headers[x])
 print("******")
-
 headers = {
     'User-Agent': 'Mobile'
 }
@@ -21,5 +30,7 @@ headers = {
 url2 = "http://httpbin.org/headers"
 rh = requests.get(url2, headers=headers)
 print(rh.text)
+
+
 
 
